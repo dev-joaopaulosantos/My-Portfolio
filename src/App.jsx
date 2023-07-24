6fbcdca95e39a59c8243fd8b15e6dae695f094ff
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-07-24 00:51:42 -0300

    fix: removido bug de render

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
 import NavBar from './components/NavBar/NavBar'
 import Home from './pages/Home/Home'
-import './app.css'
 import './variables.css'
 import './main.css'
 
